1f0d2a2da0999f9f3ba39a27a45234d9b96f5e3f
Edson <edson.kerneski@escola.pr.gov.br>
2025-06-02 12:18:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ULTRAKILL',
+    'Qual é o nome do protagonista de ULTRAKILL?',
+    'Gabriel (Você não controla o protagonista)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
